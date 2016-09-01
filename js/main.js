22f1a5d5a3192c49fcc3e1805dd2f7d54c222096
Shutko Dmitry <ds@proverstka.com>
2016-09-01 10:54:48 +0300

    zadanie 1

--- js/main.js
+++ js/main.js
@@ -9,21 +9,23 @@ $(document).ready(function(){
 		$('.second').text(seconds);
 	}, 100);
 
-	getButtonId();
+	// ДЗ с кнопками не получилось :(
 
-	function getButtonId() {
-		var buttonArr = [];
-		var size = $('.button-list button').size(); //берем колличество кнопок
-		console.log(size);
-		for (var i = 0; i < size; i++){
-			// var item = $('.button-list button').eq(i); //берем колличество лишек
-			// item.addClass('item' + item);
-			buttonArr.push(i);
-		}
-		$('button').click(function() {
-			var i = $(this).eq();
-			console.log(i);
-		});
-		console.log(buttonArr);
-	}
+	// getButtonId();
+
+	// function getButtonId() {
+	// 	var buttonArr = [];
+	// 	var size = $('.button-list button').size(); //берем колличество кнопок
+	// 	console.log(size);
+	// 	for (var i = 0; i < size; i++){
+	// 		// var item = $('.button-list button').eq(i); //берем колличество лишек
+	// 		// item.addClass('item' + item);
+	// 		buttonArr.push(i);
+	// 	}
+	// 	$('button').click(function() {
+	// 		var i = $(this).eq();
+	// 		console.log(i);
+	// 	});
+	// 	console.log(buttonArr);
+	// }
 });
